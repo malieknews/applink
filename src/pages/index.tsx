489d65347262f5eae5fed611d1e4e88e020e1526
Maliek2 <104944194+Maliek2@users.noreply.github.com>
2022-09-23 11:54:32 +0200

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,10 @@ export default function Index() {
       <div className="container">
         <div>
           <h1>
-            Hi, We're Next.js & Netlify<span className="fancy">.</span>
+            Hi, We're Maliek News<span className="fancy">.</span>
           </h1>
-          <span className="handle">@nextjs-netlify-blog</span>
-          <h2>A blog template with Next.js and Netlify.</h2>
+          <span className="handle">@MaliekNews</span>
+          <h2>A news blog template with Maliek Designs and Netlify.</h2>
           <SocialList />
         </div>
       </div>
