7730d32938f4607c7ef3356b1aed83821a072652
Maliek2 <104944194+Maliek2@users.noreply.github.com>
2022-09-29 23:37:36 +0200

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -57,19 +57,7 @@ export default function Index() {
           }
         }
       `}</style>
-       <script> 
-(function() { // REQUIRED CONFIGURATION VARIABLE: EDIT THE SHORTNAME BELOW
-var d = document, s = d.createElement('script'); // IMPORTANT: Replace EXAMPLE with your forum shortname!
-s.src = 'https://maliek101.disqus.com/recommendations.js'; s.setAttribute('data-timestamp', +new Date());
-(d.head || d.body).appendChild(s);
-})();
-</script>
-<noscript>
-Please enable JavaScript to view the 
-<a href="https://disqus.com/?ref_noscript" rel="nofollow">
-comments powered by Disqus.
-</a>
-</noscript>
+       
     </Layout>
   );
 }
